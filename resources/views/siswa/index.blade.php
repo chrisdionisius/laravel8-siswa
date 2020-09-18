859934ced48325de76df3084c2aa1dcde2a4e916
Chris Dionisius <chrisdionisius@gmail.com>
2020-09-19 01:55:12 +0700

    commit pertama

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -78,7 +78,6 @@
                             <label for="alamat">Alamat</label>
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
-
                         
                         
                 </div>
